e9194093ad4a1eb2ba13c4eb1184922a2853b524
Danica Tiu <danica.tiu@gmail.com>
2011-12-11 21:42:03 -0600

    all CSVs updated

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -5,6 +5,13 @@ class RecommendationsController < ApplicationController
   def create
     pairings = Pairing.scoped
     
+    # create new array from params hash values
+    # for each element in the array, run the pairings search
+    # save the results of each pairings search in a new array
+    # compare arrays with "&" intersection operator
+    
+    # @c = params[:pairing_descriptors].flatten.count
+    
     params[:pairing_descriptors].each do |descriptor_id|
       pairings = pairings.where(:pairing_descriptor_id => descriptor_id)
     end
